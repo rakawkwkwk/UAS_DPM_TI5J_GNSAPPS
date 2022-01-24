be0448f05ad6e1cfd314bd0be4efd847ff5bd90a
rakawkwkwk <1911500066@mahasiswa.atmaluhur.ac.id>
2022-01-24 23:15:17 +0700

    ac.atmaluhur.topuppulsa.gnsapps

---
+++ app/src/main/java/com/example/myapplication/IsipulsaActivity.java
@@ -0,0 +1,26 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class IsipulsaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_isipulsa);
+    }
+    public void onBackClick(View view){
+        startActivity(new Intent(this,DashbordActivity.class));
+        overridePendingTransition(R.anim.slide_in_left,android.R.anim.slide_out_right);
+
+    }
+    public void onDashbordClick(View View){
+        startActivity(new Intent(this,DashbordActivity.class));
+        overridePendingTransition(R.anim.slide_in_right,R.anim.stay);
+
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onLoginClick(View view){
+        startActivity(new Intent(this,LoginActivity.class));
+        overridePendingTransition(R.anim.slide_in_left,android.R.anim.slide_out_right);
+
+    }
+}
